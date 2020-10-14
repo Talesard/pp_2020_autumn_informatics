d2033c69a6b839af3946a0f1ef9fac78159f66f6
Talesard <61109747+Talesard@users.noreply.github.com>
2020-10-14 14:34:35 +0300

    fix rank

--- modules/task_1/napylov_e_different_characters/main.cpp
+++ modules/task_1/napylov_e_different_characters/main.cpp
@@ -6,11 +6,15 @@
 int main(int argc, char** argv) {
     const char* str1 = "Hello, world!";
     const char* str2 = "hello, human!***";
-    std::cout << str1 << std::endl;
-    std::cout << str2 << std::endl;
     MPI_Init(NULL, NULL);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int diff_count = getParallelDifferenceCount(str1, str2);
-    std::cout << diff_count << std::endl;
+    if (rank == 0){
+        std::cout << str1 << std::endl;
+        std::cout << str2 << std::endl;
+        std::cout << diff_count << std::endl;
+    }
     MPI_Finalize();
     return 0;
 }
